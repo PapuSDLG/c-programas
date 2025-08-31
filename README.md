37bb06ca32c770d0ec75578f95c783896b1246da
Kevin <droidekyp956@gmail.com>
2025-08-30 21:41:56 -0300

    Almost finished the hangman game

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ Trabajos que hice con C
 - Se usan punteros por si se quiere usar como parametro un array 
 - Los arrays terminan con un caracter nulo '\0'
 - Para agregar elementos al array usando una funcion se tiene que inicializar primero la variable (tuve que inicializarlo antes de asignarle los elementos a la variable)
+- Si no le das un espacio antes del %c en un scanf ocasionara problemas en los bucles como una doble iteracion 
+- Usar system("clear") para eliminar lo que se muestra en la consola
 # Notas
 - Me gustaria haber encontrado una manera mas eficiente para lo del numero maximo 64 aunque fue divertido hacerlo siento que el codigo podria optimizarse aun mas :/
--- hangman-game.c
+++ hangman-game.c
@@ -4,10 +4,15 @@
 1. Palabra para el ahorcado (HECHO)
 2. Contador de vidas (HECHO)
 3. la cantidad de letras de la palabra que empiecen con _ (HECHO)
-4. Por cada letra fallida se disminuye la cantidad de vidas y se agregara al almacen
+4. Por cada letra fallida se disminuye la cantidad de vidas (HECHO) 
+5. letra fallida se agregara al almacen (HECHO)
+6. solo se permita un caracter
+7. Mensaje al ganar (HECHO)
+
 */
 #include <stdio.h>
-
+#include <stdbool.h>
+#include <stdlib.h>
 int cantLetras(char *str) {
     int contador = 0;
     while(str[contador] != '\0') {
@@ -20,32 +25,61 @@ int cantGuiones(int cantidadLetras, char *guiones) {
         guiones[i] = '_';
     }
 }
+
+void mostrarEquivocaciones(char* array, int indiceEquivocacion) {
+    for (int i = 0; i < indiceEquivocacion; i++) {
+        printf("%c ", array[i]);
+    }
+}
 int main() {
-    char palabra[100] = "muerte";
-    char letraEquivocada[100] = {};
+    char palabra[100] = "colombia";
+    char letraIncorrecta[100] = {};
     int vidas = 5, cantidadLetras = cantLetras(palabra);
     char guiones[cantidadLetras];
     guiones[cantidadLetras] = cantGuiones(cantidadLetras, guiones);
-
-    for (int i = 0; i < cantidadLetras; i++) {
-        printf("%c ", guiones[i]);
-    }
+    bool hayCoincidencia;
+    int letrasAcertadas = 0, equivocadasIndice = 0;
     // Segun la cantidad de letras seran las veces que se imprimira "_"
-    // for (int i = 0; i < cantidadLetras; i++) {
-    //     printf("%c ", guiones[i]); // Se ira modificando segun cuanto acierte
-    
-    // }
-    // printf("\n");
-    // // Empieza el juego
-    // while (vidas != 0) {
-    //     char letra;
-    //     scanf("%c", &letra);
-    //     for (int i = 0; i < cantidadLetras; i++) {
-    //         // Si la letra es igual que reemplace el _ por la letra
-    //         if (letra == palabra[i]) {
-    //             guiones[i] = letra;
-    //         }
-    //     }
-    // }
+
+    // Empieza el juego
+    while (vidas > 0) {
+        system("clear");
+        printf("Tienes %d vidas!\n", vidas);
+        for (int i = 0; i < cantidadLetras; i++) {
+            printf("%c ", guiones[i]); // Se ira modificando segun cuanto acierte
+        }
+        printf("\nLetras erroneas: ");
+        mostrarEquivocaciones(letraIncorrecta, equivocadasIndice);
+        // Si acertaste la palabra
+        if (letrasAcertadas == cantidadLetras) {
+            system("clear");
+            printf("\n%s", palabra);
+            printf("\nHas ganado\n");
+            break;
+        }
+        printf("\n");
+        char letra;
+        
+        scanf(" %c", &letra); // se agrega un espacio antes para que no exista doble iteracion
+        for (int i = 0; i < cantidadLetras; i++) {
+            // Si la letra es igual que reemplace el _ por la letra
+            if (letra == palabra[i]) {
+                guiones[i] = letra;
+                hayCoincidencia = true;
+                letrasAcertadas++;
+            }
+        }
+        if (hayCoincidencia == false) {
+            letraIncorrecta[equivocadasIndice++] = letra;
+            vidas--;
+        }
+        if (vidas == 0) {
+            system("clear");
+            printf("Te quedaste sin vidas.\n");
+            break;
+        }
+        hayCoincidencia = false;
+    }
+    printf("Fin del juego\n");
     return 0;
 }
